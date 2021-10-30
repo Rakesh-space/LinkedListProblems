abf6e2640233d540d1fb324a056dcbc3bf971044
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-30 17:29:08 +0530

    Sorting Linked List Accending order

--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -6,16 +6,15 @@ namespace LinkedListProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n UC9LinkedListProblem..");
-            UC9DeleteAtPossi mylist9 = new UC9DeleteAtPossi();
-            Console.WriteLine("\n deleteAfter Operation Performed here:");
-            mylist9.insert(56);
-            mylist9.insert(30);
-            mylist9.insert(40);
-            mylist9.insert(70);
-            //   mylist9.DisplayAllEle();
-            mylist9.deleteAtPossi(3);
-            mylist9.DisplayAllEle();
+            Console.WriteLine("\n UC10LinkedListProblem..");
+            UC10AccendingOrder mylist10 = new UC10AccendingOrder();
+            Console.WriteLine("\n Accending order Operation Performed here:");
+            mylist10.insert(56);
+            mylist10.insert(30);
+            mylist10.insert(40);
+            mylist10.insert(70);
+            mylist10.sortedInsert();
+            mylist10.DisplayAllEle();
         }
     }
 }
