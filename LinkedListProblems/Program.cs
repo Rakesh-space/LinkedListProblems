9bc7ec4297ba2268e8d0e970b14b30bea53ac85b
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-30 17:16:39 +0530

    Adding Linked list insert after

--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -6,15 +6,14 @@ namespace LinkedListProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n UC7LinkedListProblem..");
-            UC7SearchOperation mylist7 = new UC7SearchOperation();
+            Console.WriteLine("\n UC8LinkedListProblem..");
+            UC8InsertAfter mylist8 = new UC8InsertAfter();
             Console.WriteLine("\n insertAfter Operation Performed here:");
-            mylist7.insert(56);
-            mylist7.insert(30);
-            mylist7.insert(70);
-            mylist7.DisplayAllEle();
-            mylist7.Search(30);
-            mylist7.DisplayAllEle();
+            mylist8.insert(56);
+            mylist8.insert(30);
+            mylist8.insert(70);
+            mylist8.insertAfter(mylist8.head.next, 40);
+            mylist8.DisplayAllEle();
         }
     }
 }
