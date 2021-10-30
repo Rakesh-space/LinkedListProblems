f3f349b01ff68866e6a79bf75cbff5330d4a2965
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-30 16:14:50 +0530

    Adding Welcome message

---
+++ LinkedListProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to LinkedList Problems..");
+        }
+    }
+}
